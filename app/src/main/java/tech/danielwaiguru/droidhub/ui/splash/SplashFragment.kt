acb5a2d494964c661feb87ad7beb9721a45b2bde
Daniel Waiguru <danielirungu91@gmail.com>
2020-12-12 10:17:23 +0300

    feat: customize app splash screen

--- app/src/main/java/tech/danielwaiguru/droidhub/ui/splash/SplashFragment.kt
+++ app/src/main/java/tech/danielwaiguru/droidhub/ui/splash/SplashFragment.kt
@@ -16,19 +16,19 @@ class SplashFragment : Fragment() {
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSplashBinding.inflate(layoutInflater)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //getCurrentSession()
         Handler(Looper.getMainLooper()).postDelayed({
             getCurrentSession()
         }, 1000)
+        
     }
     private fun getCurrentSession() {
         val currentUser = Firebase.auth.currentUser
